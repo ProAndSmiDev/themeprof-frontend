a0306ab083ebe72cb178726949ab0fc9392e4e2b
And Smi <65821082+ProAndSmiDev@users.noreply.github.com>
2025-08-19 21:30:06 +0300

    Libs: Настройка проекта для работы

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import Hello from "~~/components/Hello.vue";
+</script>
+
+<template>
+  <div class="app">
+    <NuxtLayout />
+  </div>
+</template>
+
+<style scoped src="css/main.css"></style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home">
+    Это главная страница сайта
+  </div>
+</template>
+
+<style scoped>
+
+</style>
