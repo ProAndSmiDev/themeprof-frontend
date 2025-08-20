6e77126d2e67bda03144d75f1a781523d22ad158
AndSmi <developer@andsmi.ru>
2025-08-20 20:19:43 +0300

    Main: Сделан первый экран с кнопкой

--- components/home/firstScreen/HomeFirstScreen.vue
+++ components/home/firstScreen/HomeFirstScreen.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <UiFirstScreenPuzzle class="home-first-screen">
@@ -13,6 +11,16 @@
       <IconTickSquare class="home-first-screen__icon"/>
       <span class="home-first-screen__text">Ещё никогда не было так просто сделать заказ</span>
     </p>
+
+    <UiLink
+        href="/products"
+        title="Заказать продукт"
+        class="home-first-screen__link"
+    >
+      <template #icon>
+        <IconArrowUpRight />
+      </template>
+    </UiLink>
   </UiFirstScreenPuzzle>
 </template>
 
