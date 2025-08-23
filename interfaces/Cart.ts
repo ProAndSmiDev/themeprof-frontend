2a5add3f624b0fbfe4e563476b25157a6cfa7a59
AndSmi <developer@andsmi.ru>
2025-08-23 15:24:53 +0300

    Cart: Сделан блок с продуктами

---
+++ interfaces/Cart.ts
@@ -0,0 +1,16 @@
+import type {ProductsPhoto} from "interfaces/Products";
+
+interface CartTariff {
+  icon: string
+  name: string
+}
+
+export interface Cart {
+  id: number
+  photo: ProductsPhoto
+  name: string
+  tags: string[]
+  description: string
+  tariff: CartTariff
+  price: number
+}
